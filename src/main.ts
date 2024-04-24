bf118a4571c220800443951f7654ac743b0a90ec
Alexandra Wittmann <alex@super-code.de>
2024-04-24 13:51:48 +0200

    feat: change main

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import './style.css';
 import './01_day/Intro-TS-Level-1_1/intro';
 import './01_day/Intro-TS-Level-1_2/intro';
 
-console.log('main.ts is connected');
+console.log('main.ts is connected...');
